e43f9b8a86192ddaa1ed3ee0d2d633b26d0c8f0c
akrrich <ayronovo@gmail.com>
2025-09-07 05:21:45 -0300

    avances boomerang

--- Assets/Scripts/ProjectileMVC/ProjectileController.cs
+++ Assets/Scripts/ProjectileMVC/ProjectileController.cs
@@ -1,7 +1,23 @@
-using UnityEngine;
 using Photon.Pun;
 
 public class ProjectileController : MonoBehaviourPun
 {
+    private ProjectileModel projectileModel;
+    private ProjectileView projectileView;
 
+    public ProjectileModel ProjectileModel { get => projectileModel; }
+    public ProjectileView ProjectileView { get => projectileView; }
+
+
+    void Awake()
+    {
+        GetComponents();
+    }
+
+
+    private void GetComponents()
+    {
+        projectileModel = GetComponent<ProjectileModel>();
+        projectileView = GetComponent<ProjectileView>();
+    }
 }

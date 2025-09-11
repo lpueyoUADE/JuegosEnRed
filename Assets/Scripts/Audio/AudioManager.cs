4e78f5e86eb4268334ea5f5f6ab00519172c3fc5
Luciano Pueyo <l.pueyo@bue.edu.ar>
2025-09-11 03:01:53 -0300

    Agrego walking y algunas cosas de UI

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -40,7 +40,7 @@ public class AudioManager : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            //DontDestroyOnLoad(gameObject);
         }
         else
         {
